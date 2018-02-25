a2b34fefb075296dc607946269e5ad7b8b67352f
Francis Sunday <iamfrankcute@gmail.com>
2018-02-25 14:59:55 +0100

    :sparkle: Tripple 3DES encryption and decryption
    added request service object

--- crypto.go
+++ crypto.go
@@ -1,5 +1,97 @@
 package ravepay
 
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/des"
+	"encoding/hex"
+	"errors"
+)
+
 type CryptoService struct {
 	Client *Client
+}
+
+// TripleDesEncrypt uses the Tripple Digital Encryption Standard to encrypt the transaction data
+// len(key) % 8 = 0
+func (c *CryptoService) TripleDesEncrypt(origData, key string) (string, error) {
+	// call main Encryption method
+	return tripleDesEncrypt(origData, []byte(key), PKCS5Padding)
+}
+
+// TripleDesEncrypt uses the Tripple Digital Encryption Standard to decrypt encrypted transaction data
+// len(key) % 8 = 0
+func (c *CryptoService) TripleDesDecrypt(encrypted, key string) (string, error) {
+	// call main Encryption method
+	return tripleDesDecrypt(encrypted, []byte(key), PKCS5UnPadding)
+}
+
+func tripleDesEncrypt(origData string, key []byte, paddingFunc func([]byte, int) []byte) (string, error) {
+	iv := []byte(key[:des.BlockSize])
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return "", err
+	}
+	orig := paddingFunc([]byte(origData), block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	crypted := make([]byte, len(orig))
+	blockMode.CryptBlocks(crypted, orig)
+	return hex.EncodeToString(crypted), nil
+}
+
+func tripleDesDecrypt(encrypted string, key []byte, unPaddingFunc func([]byte) []byte) (string, error) {
+	e, err := hex.DecodeString(encrypted)
+	if err != nil {
+		return "", err
+	}
+	iv := key[:des.BlockSize]
+
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return "", err
+	}
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	origData := make([]byte, len(e))
+	blockMode.CryptBlocks(origData, e)
+	origData = unPaddingFunc(origData)
+	if string(origData) == "unpadding error" {
+		return "", errors.New("unpadding error")
+	}
+	return string(origData), nil
+}
+
+func ZeroPadding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{0}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func ZeroUnPadding(origData []byte) []byte {
+	return bytes.TrimFunc(origData,
+		func(r rune) bool {
+			return r == rune(0)
+		})
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	if length < unpadding {
+		return []byte("unpadding error")
+	}
+	return origData[:(length - unpadding)]
+}
+
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	return PKCS5Padding(ciphertext, blockSize)
+}
+
+func PKCS7UnPadding(origData []byte) []byte {
+	return PKCS5UnPadding(origData)
 }
--- request.go
+++ request.go
@@ -1,5 +1,123 @@
 package ravepay
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 type RequestService struct {
-	Client *Client
+	Client  *Client
+	BaseURL string
+	Data    map[string]string
+}
+
+// Response is populated and sent on each request
+type Response struct {
+	StatusCode           int                    `json:"statusCode"`
+	ResponseCode         string                 `json:"responseCode"`
+	ResponseMessage      string                 `json:"responseMessage"`
+	ResponseData         map[string]interface{} `json:"responseData"`
+	RequiresValidation   bool                   `json:"requiresValidation"`
+	IsSuccessfulResponse bool                   `json:"isSuccessfulResponse"`
+}
+
+// return server response if status code is not 500
+func (res *Response) getResponseCode() string {
+	return res.ResponseCode
+}
+
+// return the server response status code
+func (res *Response) getStatusCode() int {
+	return res.StatusCode
+}
+
+// confirm response was sent
+func (res *Response) isSuccessfulResponse() bool {
+	return res.IsSuccessfulResponse
+}
+
+/**
+ * some request to server will be successful but requires
+ * a validation step to complete action. use requiresValidation to
+ * check if you need to validate
+ */
+func (res *Response) requiresValidation() bool {
+	return res.RequiresValidation
+}
+
+// return the request's response message
+func (res *Response) getResponseMessage() string {
+	return res.ResponseMessage
+}
+
+// return response data
+func (res *Response) getResponseData() map[string]interface{} {
+	return res.ResponseData
+}
+
+// NewRequest acts as the constructor to make subsequent requests from the sdk
+func (r *RequestService) NewRequest(url string) *RequestService {
+	r.BaseURL = url
+	r.Data = make(map[string]string)
+	return r
+}
+
+func (r *RequestService) AddBody(key, value string) {
+	r.Data[key] = value
+}
+
+func (r *RequestService) GetBody() map[string]string {
+	return r.Data
+}
+
+func (r *RequestService) DoRequest() (*Response, error) {
+	rs := Response{}
+	r.Client.httpClient = &http.Client{Timeout: 60}
+	req, err := r.Client.newRequest("POST", r.BaseURL, r.Data, nil)
+	if err != nil {
+		return &rs, err
+	}
+	resp, err := r.Client.do(req, &rs)
+	if err != nil {
+		return &rs, err
+	}
+
+	return r.parseResponse(resp), nil
+}
+
+func (r *RequestService) parseResponse(response *http.Response) *Response {
+	resp := Response{}
+	data := make(map[string]interface{})
+	resp.StatusCode = response.StatusCode
+	if response.StatusCode < 500 {
+		resBytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if err := json.Unmarshal(resBytes, &data); err != nil {
+			log.Fatalln("Error: %s\n", err)
+		}
+		resp.ResponseData = data
+		if data["data"].(map[string]interface{})["responseCode"] != "" {
+			resp.ResponseCode = data["data"].(map[string]interface{})["responseCode"].(string)
+		}
+		if data["data"].(map[string]interface{})["responsecode"] != "" {
+			resp.ResponseCode = data["data"].(map[string]interface{})["responsecode"].(string)
+		}
+
+		if resp.ResponseCode != "" && resp.ResponseCode == "02" {
+			resp.RequiresValidation = true
+		}
+
+		if data["status"] == "success" && resp.StatusCode == 200 && (resp.ResponseCode == "00" || resp.ResponseCode == "0" || resp.ResponseCode == "02") {
+			resp.IsSuccessfulResponse = true
+		}
+
+		if data["data"].(map[string]interface{})["resposemessage"] != "" {
+			resp.ResponseMessage = data["data"].(map[string]interface{})["responsemessage"].(string)
+		}
+	}
+	return &resp
 }
